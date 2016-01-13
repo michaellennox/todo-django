d3b5fc35bd507417437edc239caed623ee6a8c65
michaellennox <michaelctlennox@gmail.com>
2016-01-13 19:33:45 +0000

    Adds rendering of content sent via POST to home page

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -5,7 +5,7 @@ from django.http import HttpRequest
 
 from lists.views import home_page
 
-class SmokeTest(TestCase):
+class HomePageTest(TestCase):
 
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
@@ -14,5 +14,18 @@ class SmokeTest(TestCase):
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
-        expected_html = render_to_string('home.html')
+        expected_html = render_to_string('home.html', request=request)
+        self.assertEqual(response.content.decode(), expected_html)
+
+    def test_home_page_can_save_a_POST_request(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'A new list item'
+        response = home_page(request)
+        self.assertIn('A new list item', response.content.decode())
+        expected_html = render_to_string(
+                'home.html',
+                {'new_item_text': 'A new list item'},
+                request=request
+        )
         self.assertEqual(response.content.decode(), expected_html)
