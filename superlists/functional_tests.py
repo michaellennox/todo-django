b54867c76f6140426a6d9cf74ad2c61109858769
michaellennox <michaelctlennox@gmail.com>
2016-01-13 16:28:43 +0000

    Extends functional test with unittest package

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,37 +1,48 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
+class NewVisitorTest(unittest.TestCase):
 
-# Edith has heard about a cool new online to-do app. 
-# She goes to check out its homepage
-browser.get('http://localhost:8000')
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+    
+    def tearDown(self):
+        self.browser.quit()
 
-# Well I guess were doing a todo....
-assert 'To-Do' in browser.title
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # Edith has heard about a cool new online to-do app. 
+        # She goes to check out its homepage
+        self.browser.get('http://localhost:8000')
 
-# She is invited to enter a to-do item straight away
+        # Well I guess were doing a todo....
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
 
-# She types "Buy peacock feathers" into a text box
-# That's one weird thing to type
+        # She is invited to enter a to-do item straight away
 
-# When she hits enter, the page updates, and now the page lists
-# "1: Buy peacock feathers" as an item in a to-do list
+        # She types "Buy peacock feathers" into a text box
+        # That's one weird thing to type
 
-# There is still a text box inviting her to add another item.
-# She enters "Use peacock feathers to make a fly"
+        # When she hits enter, the page updates, and now the page lists
+        # "1: Buy peacock feathers" as an item in a to-do list
 
-# Surprisingly! The page updates again and has BOTH items
-# oh gosh golly
+        # There is still a text box inviting her to add another item.
+        # She enters "Use peacock feathers to make a fly"
 
-# She wonders whether the site remembers this or whether it's crap
-# Oh look! She has a unique URL for just herself
-# And there's some text to tell her about it
+        # Surprisingly! The page updates again and has BOTH items
+        # oh gosh golly
 
-# Now she goes to the URL and oh gosh darn what do you know
-# the to-do list is still there!
+        # She wonders whether the site remembers this or whether it's crap
+        # Oh look! She has a unique URL for just herself
+        # And there's some text to tell her about it
 
-# I wonder if that was magic or just an awesome programmer called Michael
-# (who really should get hired by someone if they've bothered reading this)
-# Satisfied, she goes back to sleep
+        # Now she goes to the URL and oh gosh darn what do you know
+        # the to-do list is still there!
 
-browser.quit()
+        # I wonder if that was magic or just an awesome programmer called Michael
+        # (who really should get hired by someone if they've bothered reading this)
+        # Satisfied, she goes back to sleep
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
