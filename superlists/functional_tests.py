291b12ba8cf642ddae2f29f89d6fec11224c07c1
michaellennox <michaelctlennox@gmail.com>
2016-01-13 21:18:11 +0000

    Extends functional test to include adding a second item to todolist

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -42,13 +42,21 @@ class NewVisitorTest(unittest.TestCase):
 
         # There is still a text box inviting her to add another item.
         # She enters "Use peacock feathers to make a fly"
-        self.fail('Finish the test!')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+
         # Surprisingly! The page updates again and has BOTH items
         # oh gosh golly
-
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn('2: Buy peacock feathers to make a fly', [row.text for row in rows])
+        
         # She wonders whether the site remembers this or whether it's crap
         # Oh look! She has a unique URL for just herself
         # And there's some text to tell her about it
+        self.fail('Finish the test!')
 
         # Now she goes to the URL and oh gosh darn what do you know
         # the to-do list is still there!
