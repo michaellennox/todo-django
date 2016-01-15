c370ba53577f3c588c24954ca5ec9ace3ad3a8a8
michaellennox <michaelctlennox@gmail.com>
2016-01-15 11:04:10 +0000

    Makes functional tests an app, renames main app

--- app/functional_tests/tests.py
+++ app/functional_tests/tests.py
@@ -1,8 +1,9 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -19,7 +20,7 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app.
         # She goes to check out its homepage
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
 
         # Well I guess were doing a todo....
         self.assertIn('To-Do', self.browser.title)
@@ -66,6 +67,3 @@ class NewVisitorTest(unittest.TestCase):
         # I wonder was that magic or just an awesome programmer called Michael
         # (who really should get hired by someone if they've read this)
         # Satisfied, she goes back to sleep
-
-if __name__ == '__main__':
-    unittest.main(warnings='ignore')
